d1029dcba165fc3b40309ec40ac345ea0e4ca362
sunboyy <sura_sun@hotmail.com>
2020-11-15 10:07:30 +0700

    Add breadcrumbs and initial responsiveness

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NzButtonModule, NzIconModule, NzInputModule, NzSelectModule } from 'ng-zorro-antd';
 import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
 import { AppComponent } from './app.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { NavComponent } from './nav/nav.component';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, BreadcrumbsComponent],
   imports: [
     BrowserModule,
     FormsModule,
