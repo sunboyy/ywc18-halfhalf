1497f8ccfb94c5db160717e38275e4c7ba086098
sunboyy <sura_sun@hotmail.com>
2020-11-19 22:00:51 +0700

    Remove HTTP server proxy

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { DataModel, DataService } from '../data.service';
+import { DataModel, DataService, Merchant } from '../data.service';
 import { Filter } from '../filter/filter.component';
 
 @Component({
@@ -24,5 +24,22 @@ export class ResultComponent implements OnInit {
     this.dataService.getData().subscribe((data) => {
       this.data = data;
     });
+  }
+
+  get subcategory(): string[] {
+    if (this.filter.subcategory === 'all') {
+      return this.data.categories.find((category) => category.name === this.filter.category).subcategories;
+    }
+    return [this.filter.subcategory];
+  }
+
+  get filteredMerchants(): Merchant[] {
+    return this.data.merchants.filter(
+      (merchant) =>
+        (this.filter.province === 'nearby' || merchant.addressProvinceName === this.filter.province) &&
+        (['', 'all'].includes(this.filter.priceRange) ||
+          merchant.priceLevel === parseInt(this.filter.priceRange, 10)) &&
+        (this.filter.category === 'all' || this.subcategory.includes(merchant.subcategoryName))
+    );
   }
 }
